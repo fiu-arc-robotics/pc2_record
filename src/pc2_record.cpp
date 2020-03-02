a8baa4491f6597c447ec0b0002463550b0c96758
Enmar123 <js_enmar1@hotmail.com>
2020-03-02 15:38:30 -0500

    add record toggle

--- src/pc2_record.cpp
+++ src/pc2_record.cpp
@@ -1,9 +1,10 @@
-//#include <iostream>
 #include <fstream>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_ros/point_cloud.h>
-//#include <pcl_conversions/pcl_conversions.h>
+//#include <std_msgs/Empty.h>
+#include <std_msgs/Bool.h>
+
  
 typedef pcl::PointCloud<pcl::PointXYZRGB> PCLCloud;
 pcl::PCLPointCloud2::Ptr pcl_pc2_temp(new pcl::PCLPointCloud2);
@@ -11,10 +12,12 @@ PCLCloud::Ptr temp_cloud(new PCLCloud);
 
 std::ofstream myfile;
 
-std::string pc2_topic_in = "/rplidar/cloud_color";
-std::string save_as = "/home/arc/clouds/test/cloud.txt";
+std::string pc2_topic_in =  "cloud_color";
+std::string bool_topic_in = "is_PauseRecording";
+std::string save_as = "~/Desktop/cloud.txt";
 
 uint n_point = 0;   // number of points written
+bool is_PauseRecording = false;
 
 void writeXYZRGB() {
     // Writes xyzrgb data to a text file line by line
@@ -39,10 +42,16 @@ void writeXYZRGB() {
 }
 
 void pc2Callback( const sensor_msgs::PointCloud2::ConstPtr& msg ) {
-    // Conver the ros msg into a usable form
-    pcl_conversions::toPCL(*msg, *pcl_pc2_temp);
-    pcl::fromPCLPointCloud2(*pcl_pc2_temp, *temp_cloud);
-    writeXYZRGB();
+    if(is_PauseRecording != true) {
+        // Conver the ros msg into a usable form
+        pcl_conversions::toPCL(*msg, *pcl_pc2_temp);
+        pcl::fromPCLPointCloud2(*pcl_pc2_temp, *temp_cloud);
+        writeXYZRGB();
+    }
+}
+
+void boolCallback( const std_msgs::Bool::ConstPtr& msg ) {
+    is_PauseRecording = msg->data;
 }
 
 
@@ -51,14 +60,17 @@ int main( int argc, char **argv ) {
     
     ROS_INFO( "pc2_record: Starting Node" );
     ros::NodeHandle nh("~");
-    ros::Subscriber sub;
+    ros::Subscriber sub_pc2;
+    ros::Subscriber sub_bool;
     
     ROS_INFO( "pc2_record: Obtaining Params" );
     nh.getParam("pc2_topic_in", pc2_topic_in );
+    nh.getParam("bool_topic_in", bool_topic_in );
     nh.getParam("save_as", save_as );
     
     ROS_INFO( "pc2_record: Configuring Node" );
-    sub = nh.subscribe( pc2_topic_in, 10, pc2Callback );
+    sub_pc2 = nh.subscribe( pc2_topic_in, 10, pc2Callback );
+    sub_bool = nh.subscribe( bool_topic_in, 10, boolCallback); 
     
     ROS_INFO( "pc2_record: Starting Spin" );
     ros::spin();
